eeb932c6743b73ded8f3786f6595a1ce61bfb47d
Dipershan Shrestha <dipsestha321@gmail.com>
2024-06-03 17:33:07 +0545

    bootstrap

--- html/index.js
+++ html/index.js
@@ -1,22 +1,61 @@
-const   formsSumission= ()=>{
-    const formData = document.getElementById("hello");
-    const formDatas =  new FormData(formData);
-    //Display Values
-    for(const value of formDatas.values()){
-        console.log(value);
-    }
-    document.getElementById("contactForm").reset();
-    document.getElementsByClassName("systemMsg")[0].innerHTML = 
-        "Thanku for contacting us";
-    setTimeout(()=>{
-        document.getElementsByClassName("systemMsg")[0].innerHTML = 
-        "";
-    } , 3000)
+// const   formsSumission= ()=>{
+//     const formData = document.getElementById("hello");
+//     const formDatas =  new FormData(formData);
+//     //Display Values
+//     for(const value of formDatas.values()){
+//         console.log(value);
+//     }
+//     document.getElementById("contactForm").reset();
+//     document.getElementsByClassName("systemMsg")[0].innerHTML = 
+//         "Thanku for contacting us";
+//     setTimeout(()=>{
+//         document.getElementsByClassName("systemMsg")[0].innerHTML = 
+//         "";
+//     } , 3000)
 
-};
+// };
 
-[{
-    innerHTML:"",
-},
-]
+// [{
+//     innerHTML:"",
+// },
+// ]
+
+
+//Input OTP code has been sent to 9863167766
+//Input OTP code has been sent to ********66
+
+
+// const str =" Input OTP code has been sent to 9863167766 ";
+//Change The Input: 9860335785 To Output: ********85
+
+
+
+
+
+
+//   const changedNumber = (number) => {
+//     if ((number.length === 10)) {
+//       console.log( "********" + number.slice(8, 10));
+//     } 
+//     else {
+//       console.log("Invalid number");
+//     }
+//   };
+// console.log(changedNumber("9863167766")) 
+
+//Ternary Operator
+const number =  "9863167766" ;
+const result  = 
+number.length === 10
+? console.log( "********" + number.slice(8, 10))
+:"Invalid number";
+
+
+
+
+// let arr1 = ["Momo", "Pizza", "Ice-cream"];
+// let arr2 = ["Fruits", "Veggies", "Pizza"];
+ 
+
+
 

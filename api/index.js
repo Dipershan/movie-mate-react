aa6fc4282a3b94595a0fbbe33f667dd043eb8183
Dipershan Shrestha <dipsestha321@gmail.com>
2024-06-05 17:06:04 +0545

    day 3 bootsrap

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const getProducts = async (url) =>{
+    /**
+     * fetch(url)
+     * .then((d)=> d.json())
+     * .then((result) => console.log({result}))
+     * .catch(error => console.log(error));
+     */
+    try {
+        const d =  await fetch(url);
+        const result = await d.json();
+        console.log({result});
+    } catch (error) {
+        console.log(error);
+    }
+}

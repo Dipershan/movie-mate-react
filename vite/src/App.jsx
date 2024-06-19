8bfd07f8830bf6cf37b8f2345f3bac383e6c98da
Dipershan Shrestha <dipsestha321@gmail.com>
2024-06-19 15:01:25 +0545

    reactjsx

---
+++ vite/src/App.jsx
@@ -0,0 +1,9 @@
+import Goal from "./Goal";
+
+export const App = () => {
+  return (
+    <div><Goal /></div>
+  );
+}
+
+export default App;
---
+++ vite/src/Goal.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const Goal = () => {
+
+  //js function
+
+  const shoot  = (data) =>{
+    alert(`goal by ${data}`);
+  };
+  return (
+    <div>
+        <input placeholder='NAme'  type="text" name="" id="rod" />
+
+        <select name="" id="siu" onChange = {()=> shoot(document.getElementById("siu").value)}>
+        <option value="">SElect </option>
+            <option value="messi">Messi</option>
+            <option value="ronaldo">Ronaldo</option>
+        </select>
+        <button onClick={()=>shoot(document.getElementById("siu").value)}>Goal</button>
+    </div>
+  )
+}
+
+export default Goal;
